99c7e9d202d4ced25678b8878e7622aeb3c93458
Cola <cola14@gmail.com>
2020-11-07 15:40:04 +0100

    app store and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import Dashboard from './pages/dashboard/Dashboard';
 
-function App() {
+const theme = createMuiTheme();
+
+export const DefaultComponent = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Start Component
     </div>
-  );
+  )
 }
 
+export const App = ({store, persistor, history}) => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <MuiThemeProvider theme={theme}>
+          <ConnectedRouter history={history}>
+            <Suspense fallback={<div>loading...</div>}>
+              <Switch>
+                <Route exact path='/' component={Dashboard}/>
+                <Route path='/cards' component={DefaultComponent}/>
+                <Route path='/cards/add' component={DefaultComponent}/>
+                <Route path='/cards/:id/edit' component={DefaultComponent}/>
+              </Switch>
+            </Suspense>
+          </ConnectedRouter>
+        </MuiThemeProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
 export default App;
